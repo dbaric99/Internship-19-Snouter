65f8ef3c0e3a123c66e41a238a0f8efdcf7a5318
dbaric99 <damjana.baric1999@gmail.com>
2023-04-20 22:44:32 +0200

    Added CRUD for products

--- snouter/src/products/dto/create-product.dto.ts
+++ snouter/src/products/dto/create-product.dto.ts
@@ -1,1 +1,37 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { State } from '@prisma/client';
+
+export class CreateProductDto {
+  @ApiProperty()
+  slug: string;
+
+  @ApiProperty({ required: true })
+  name: string;
+
+  @ApiProperty({ required: false })
+  description: string;
+
+  @ApiProperty({ required: true })
+  price: number;
+
+  @ApiProperty({ required: true })
+  createdAt: Date;
+
+  @ApiProperty({ required: false, nullable: true })
+  updatedAt: Date | null;
+
+  @ApiProperty({ required: true })
+  state: State;
+
+  @ApiProperty({ required: true })
+  isSold: boolean;
+
+  @ApiProperty({ required: true })
+  subcategoryId: number;
+
+  @ApiProperty({ required: true })
+  sellerId: number;
+
+  @ApiProperty()
+  categoryId: number;
+}
--- snouter/src/products/entities/product.entity.ts
+++ snouter/src/products/entities/product.entity.ts
@@ -1,1 +1,40 @@
-export class Product {}
+import { Product, State } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProductEntity implements Product {
+  @ApiProperty({ required: true })
+  id: number;
+
+  @ApiProperty({ required: true })
+  name: string;
+
+  @ApiProperty({ required: false })
+  description: string;
+
+  @ApiProperty({ required: true })
+  price: number;
+
+  @ApiProperty({ required: true })
+  createdAt: Date;
+
+  @ApiProperty()
+  slug: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  updatedAt: Date | null;
+
+  @ApiProperty({ required: true })
+  state: State;
+
+  @ApiProperty({ required: true })
+  isSold: boolean;
+
+  @ApiProperty({ required: true })
+  subcategoryId: number;
+
+  @ApiProperty({ required: true })
+  sellerId: number;
+
+  @ApiProperty()
+  categoryId: number;
+}
