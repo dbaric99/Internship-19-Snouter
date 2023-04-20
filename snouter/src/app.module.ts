80b15a6303c2e7b432fde5457617e6dd6a7f99e9
dbaric99 <damjana.baric1999@gmail.com>
2023-04-20 12:16:08 +0200

    Created necessary modules

--- snouter/src/app.module.ts
+++ snouter/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, ProductsModule, CategoriesModule, SubcategoriesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
