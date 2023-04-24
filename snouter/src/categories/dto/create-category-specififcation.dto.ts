7eba825e38a68d377a0bef2ee8fa5dfd510be423
dbaric99 <damjana.baric1999@gmail.com>
2023-04-24 22:13:34 +0200

    Added category specification validators

--- snouter/src/categories/dto/create-category-specififcation.dto.ts
+++ snouter/src/categories/dto/create-category-specififcation.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Length, IsString, IsNotEmpty } from 'class-validator';
 
 export class CategorySpecificationDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
   @ApiProperty()
   name: string;
 }
